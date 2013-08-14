b1d5eacdd19f48495dc6512b86d67095db566287
lomsansnom <alexandre.cathalan@gmail.com>
2013-08-14 20:31:41 +0200

    debug

--- public/javascript/events/gpiomanager.js
+++ public/javascript/events/gpiomanager.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	$("[id^='pin']").on('click', function(){
 		alert("Veuillez utiliser les pins représentées en vertes")
-	})
+	});
+	
+	$("[id^='gpio']").on('click', function(){
+		$.post('/changerEtatGpio', {"numero" : $(this).attr('id').substring(4)})
+	}); 
 })
-
-$("[id^='gpio']").on('click', function(){
-	$.post('/changerEtatGpio', {"numero" : $(this).attr('id').substring(4)})
-}) 
