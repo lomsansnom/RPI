79e79b198090eca2fc4cf1b6688233b1ebf2b8c2
lomsansnom <alexandre.cathalan@gmail.com>
2013-08-18 10:21:59 +0200

    ajout de la coloration des numéros en fonction de l'état du GPIO après un changement d'état

--- public/javascript/events/gpiomanager.js
+++ public/javascript/events/gpiomanager.js
@@ -7,6 +7,10 @@ $(document).ready(function(){
 		$.post('/changerEtatGpio', {"numero" : $(this).attr('id').substring(4), "mode" : "out"}, function(ret){
 			alert("numero : " + ret['numero'])
 			alert("etat : " + ret['etat'])
+			if(ret['etat'] == 0)
+				$("#" + toLowerCase(ret['numero'])).css("color", "black")
+			else if(ret['etat'] == 1)
+				$("#" + toLowerCase(ret['numero'])).css("color", "red")
 		})
 	}); 
 })
