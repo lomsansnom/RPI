dc3f8a267f4e28944c82ea0492fdec5808efff50
lomsansnom <alexandre.cathalan@gmail.com>
2013-08-19 16:51:57 +0200

    ajout de la coloration des numéros en fonction du retour de getEtatGpios

--- public/javascript/gpiomanager.js
+++ public/javascript/gpiomanager.js
@@ -2,6 +2,21 @@
 function getEtatGpios()
 {
 	$.post("/getEtatGpios", function(ret){
-		alert(ret)
+		for(var i in ret)
+		{
+			var key = i
+			if(ret[i] == 0)
+			{
+				$("#" + ret[key].toLowerCase()).css("color", "black")
+			}
+			else if(ret[i] == 1)
+			{
+				$("#" + ret[key].toLowerCase()).css("color", "red")
+			}
+			else
+			{
+				alert("Erreur lors de l'obtention de l'état du gpio " + key)
+			}
+		}
 	})
 }
