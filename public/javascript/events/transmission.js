6cb6c51ab41e3aba9e9545c3d823dbffc56c31ff
lomsansnom <alexandre.cathalan@gmail.com>
2013-08-27 13:41:24 +0200

    debug

--- public/javascript/events/transmission.js
+++ public/javascript/events/transmission.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 			   }
 		})*/
 		var data = '"repertoire" : "' + $("[name='selDD']:checked").val() + '"'
-		$.post('/downloadTorrent', {data}, function(ret){
+		$.post('/downloadTorrent', data, function(ret){
 			   alert(JSON.stringify(ret))
 		})
 	})
