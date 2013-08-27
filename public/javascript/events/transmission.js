eb02a53851214be01befbedf5258817f4d4e67ad
lomsansnom <alexandre.cathalan@gmail.com>
2013-08-27 14:16:06 +0200

    debug

--- public/javascript/events/transmission.js
+++ public/javascript/events/transmission.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	getListeDD()
 	
 	$("#torrentOk").on('click', function(){
-		/*var data = '{"repertoire" : "' + $("[name='selDD']:checked").val() + '"}' 
+		var data = '{"repertoire" : "' + $("[name='selDD']:checked").val() + '"}' 
 		$.ajax({async : false,
 			   url : '/downloadTorrent',
 			   type : 'POST',
@@ -15,10 +15,10 @@ $(document).ready(function(){
 				   alert(JSON.stringify(ret))
 				   alert('erreur lors de l\'appel de /downloadTorrent')
 			   }
-		})*/
-		var data = '{"repertoire" : "' + $("[name='selDD']:checked").val() + '"}'
+		})
+		/*var data = '{"repertoire" : "' + $("[name='selDD']:checked").val() + '"}'
 		$.post('/downloadTorrent', {"repertoire" : $("[name='selDD']:checked").val()}, function(ret){
 			   alert(JSON.stringify(ret))
-		})
+		})*/
 	})
 })
