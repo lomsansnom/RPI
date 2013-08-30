e557b192be11f303d9ba3c08ee653dc77bd9bef1
lomsansnom <alexandre.cathalan@gmail.com>
2013-08-30 15:33:12 +0200

    le download peut maintenant être effectuer sur le RPI si le bouton radio correspondant est sélectionné

--- public/javascript/events/transmission.js
+++ public/javascript/events/transmission.js
@@ -2,7 +2,22 @@ $(document).ready(function(){
 	getListeDD()
 	
 	$("#torrentOk").on('click', function(){
-		var data = '{"repertoire" : "' + $("[name='selDD']:checked").val() + '"}' 
-		isMounted(data)
+		if($("[name='selDD']:checked").val() != 'rpi')
+		{
+			var data = '{"repertoire" : "' + $("[name='selDD']:checked").val() + '"}' 
+			isMounted(data)
+		}
+		else
+		{
+			 $.ajax({async : false, 
+				 	url : '/downloadTorrent',
+				 	type : 'POST',
+				 	contentType : 'application/json', 
+				 	data : '{"repertoire" : " /home/pi/torrents", "torrent" : "' + $("#lienTorrent").val() + '"}',
+				 	success : function(rett){
+				 		alert(rett)
+				 	}
+			 })
+		}
 	})
 })
