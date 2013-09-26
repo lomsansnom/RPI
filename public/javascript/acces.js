784fb718087a9a06f775f35e44994ae739208c55
alex <alex@localhost.localdomain>
2013-09-26 22:45:57 +0200

    debug

--- public/javascript/acces.js
+++ public/javascript/acces.js
@@ -6,14 +6,14 @@ function ajouterMembre()
 	 	contentType : 'application/json', 
 	 	data : '{"username" : "' + $("#username").val() + '", "password" : "' + $("#password").val() + '"}',
 	 	success : function(ret){
-	 		if(ret.ok)
+	 		if(ret.OK)
 	 		{
 	 			$("#main").append("Votre compte a bien été ajouté, vous allez être redirigé vers la page d'accueil")
 	 			setTimeout(function(){$(location).attr('href','/');},3000);
 	 		}
 	 		else
 	 		{
-	 			alert(ret.erreur)
+	 			alert(ret.Erreur)
 	 		}
 	 	},
 		error : function(ret){
