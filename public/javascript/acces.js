c8bd04d5d0f7cc19e41e6fd4c3a4523e8ffa5bad
alex <alex@localhost.localdomain>
2013-09-27 12:35:43 +0200

    ajout du hash pour l'ajout de membre

--- public/javascript/acces.js
+++ public/javascript/acces.js
@@ -4,7 +4,7 @@ function ajouterMembre()
 	 	url : '/ajouterMembre',
 	 	type : 'POST',
 	 	contentType : 'application/json', 
-	 	data : '{"username" : "' + $("#username").val() + '", "password" : "' + $("#password").val() + '"}',
+	 	data : '{"username" : "' + $("#username").val() + '", "password" : "' +  CryptoJS.MD5($("#password").val()).toString() + '"}',
 	 	success : function(ret){
 	 		if(ret.OK)
 	 		{
