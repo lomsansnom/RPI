4566fc50db5ca2ac813acb3d3daac312a2da832e
alex <alex@localhost.localdomain>
2013-09-30 19:52:39 +0200

    debug

--- public/javascript/lecteur.js
+++ public/javascript/lecteur.js
@@ -13,7 +13,7 @@ function getListeMusiques(chemin)
 	 				$("#listeMusiques").append("<h2>Parent</h2>")
 	 				for(var i in ret.parent)
 	 				{
-	 					if(ret.parent[i].substring(ret.parent[i].length() - 4; ret.parent[i].length()) == ".mp3")
+	 					if(ret.parent[i].substring(ret.parent[i].length() - 4, ret.parent[i].length()) == ".mp3")
 	 					{
 	 						$("#listeMusiques").append("<p>" + ret.parent[i].substring(ret.parent[i].length() - 4) + "</p>")
 	 					}
@@ -25,7 +25,7 @@ function getListeMusiques(chemin)
 		 				{
 	 						for(var ii in ret.sousDossier)
 		 					{	
-	 							if(ret.sousDossier[i][ii].substring(ret.sousDossier[i][ii].length() - 4; ret.sousDossier[i][ii].length()) == ".mp3")
+	 							if(ret.sousDossier[i][ii].substring(ret.sousDossier[i][ii].length() - 4, ret.sousDossier[i][ii].length()) == ".mp3")
 	 							{
 		 							$("#listeMusiques").append("<p>" + ret.sousDossier[i][ii].substring(ret.sousDossier[i][ii].length() - 4) + "</p>")
 		 						}
@@ -39,7 +39,7 @@ function getListeMusiques(chemin)
 		 						for(var ii in ret.sousDossier)
 			 					{
 		 							for(var iii in ret.sousSousDossier)
-			 							if(ret.sousSousDossier[i][ii][iii].substring(ret.sousSousDossier[i][ii][iii].length() - 4; ret.sousSousDossier[i][ii][iii].length()) == ".mp3")
+			 							if(ret.sousSousDossier[i][ii][iii].substring(ret.sousSousDossier[i][ii][iii].length() - 4, ret.sousSousDossier[i][ii][iii].length()) == ".mp3")
 			 							{
 				 							$("#listeMusiques").append("<p>" + ret.sousSousDossier[i][ii][iii].substring(ret.sousSousDossier[i][ii][iii].length() - 4) + "</p>")
 				 						}
